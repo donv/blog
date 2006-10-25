aa7182260b54d68dfae3645736087f0a5e330a33
Uwe Kubosch <uwe@kubosch.no>
2006-10-25 07:25:44 +0000

    * Fixed image problems

--- db/migrate/002_images.rb
+++ db/migrate/002_images.rb
@@ -2,7 +2,7 @@ class Images < ActiveRecord::Migration
   def self.up
     create_table :images do |table|
       table.column :blog_id, :integer, :null => false
-      table.column "picture_data", :binary
+      table.column "picture_data", :binary, :limit => 10000000
       table.column "picture_content_type", :string, :limit => 100
     end
   end
